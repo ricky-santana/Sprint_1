cc34c5cf4f6b614ab9a5e341840a142168dd8cd6
Richard <153839902+ricky-santana@users.noreply.github.com>
2026-06-29 12:31:39 +0200

    Clase coche del 01_2

---
+++ src/S1_01_2/Coche.java
@@ -0,0 +1,14 @@
+package S1_01_2;
+
+public class Coche {
+
+    private static final String brand = "Cupra";
+    private static String model;
+    private final int power;
+
+    public Coche(String model, int power){
+        this.model = model;
+        this.power = power;
+    }
+
+}
